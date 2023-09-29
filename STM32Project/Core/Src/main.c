e6b8669fa617dc0719fe4fb44dc850f3ad0d2491
DacNguyen99 <datnguyen2314@gmail.com>
2023-09-29 08:29:12 +0700

    Init for Ex_7

--- STM32Project/Core/Src/main.c
+++ STM32Project/Core/Src/main.c
@@ -20,6 +20,10 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+void clearAllClock(){
+	HAL_GPIO_WritePin(GPIOA, LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin|LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin|LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin, 1);
+}
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
