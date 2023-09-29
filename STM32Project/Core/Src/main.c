e5b0517c63ef42c64738461680f8bb1b5ae9c30c
DacNguyen99 <datnguyen2314@gmail.com>
2023-09-29 08:58:18 +0700

    Init for Ex_9

--- STM32Project/Core/Src/main.c
+++ STM32Project/Core/Src/main.c
@@ -20,6 +20,50 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+void clearNumberOnClock(int num){
+	switch(num){
+	case 0:
+		HAL_GPIO_WritePin(GPIOA, LED_1_Pin, 1);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(GPIOA, LED_2_Pin, 1);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(GPIOA, LED_3_Pin, 1);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(GPIOA, LED_4_Pin, 1);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(GPIOA, LED_5_Pin, 1);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(GPIOA, LED_6_Pin, 1);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(GPIOA, LED_7_Pin, 1);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(GPIOA, LED_8_Pin, 1);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(GPIOA, LED_9_Pin, 1);
+		break;
+	case 9:
+		HAL_GPIO_WritePin(GPIOA, LED_10_Pin, 1);
+		break;
+	case 10:
+		HAL_GPIO_WritePin(GPIOA, LED_11_Pin, 1);
+		break;
+	case 11:
+		HAL_GPIO_WritePin(GPIOA, LED_12_Pin, 1);
+		break;
+	default:
+		HAL_GPIO_WritePin(GPIOA, LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin|LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin|LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin, 1);
+		break;
+	}
+}
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
